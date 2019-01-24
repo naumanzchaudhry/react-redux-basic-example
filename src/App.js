4f3fa445bb945f9d2e8de1e769c3eb0be6aebedd
Nauman Zafar <naumanzafarchaudhry@gmail.com>
2019-01-24 16:39:56 +0500

    Basic example react+ redux

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
-import Layout from './components/Layout/Layout';
-import Aux from './hoc/Aux';
+import classes from './App.css';
+import {connect} from 'react-redux';
 
 
 class App extends Component {
-  state = {
-    alreadyLogedIn: true
-  }
-
-  logout = () => {
-    this.setState({
-      alreadyLogedIn: false
-    })
-  }
 
   render() {
     return (
-      <BrowserRouter>
-       <Aux> 
-        <Layout isAlreadyLoggedIn={this.state.alreadyLogedIn} 
-         logout={this.logout} />
-        <Route path="/" exact Component={App} />
-       </Aux>
-      </BrowserRouter>
+      <div>
+        <div className={classes.Header}>
+            <h1>Counter: {this.props.ctr}</h1>
+        </div>
+
+        <section className={classes.Section}>
+            <button type="button" onClick={this.props.onIncrement}>Increment</button>
+            <button type="button" onClick={this.props.onDecrement}>Decrement</button>
+            <button type="button" onClick={this.props.onAdd}>Add 10</button>
+            <button type="button" onClick={this.props.onSubtract}>Subtract 5</button>
+        </section>
+      </div>
     )
   }
 }
 
-export default App;
+const mapStateToDispatch = dispatch => {
+  return {
+    onIncrement: () => dispatch({type: 'INCREMENT'}),
+    onDecrement: () => dispatch({type: 'DECREMENT'}),
+    onAdd: () => dispatch(
+      {
+        type:'ADD',
+        payload: {
+          value: 10
+        }
+      }
+    ),
+    onSubtract: () => dispatch(
+      {
+        type:'SUBTRACT',
+        payload: {
+          value: 5
+        }
+      }
+    ),
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter
+  }
+}
+
+export default connect(mapStateToProps, mapStateToDispatch)(App);
